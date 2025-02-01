1eecbef6775917306cf39871002f93da3265528b
Bryan Rainbow <bryanrbow@gmail.com>
2025-02-01 15:39:10 -0800

    Add frostburn spells up to Bone Chill

--- Database Creation/data_insertion/Frostburn/insert_all.sql
+++ Database Creation/data_insertion/Frostburn/insert_all.sql
@@ -1,3 +1,6 @@
 
 SELECT "Inserting Frostburn Classes...";
 source data_insertion/Frostburn/insert_classes.sql;
+
+SELECT "Inserting Frostburn Spells...";
+source data_insertion/Frostburn/insert_spells.sql;
